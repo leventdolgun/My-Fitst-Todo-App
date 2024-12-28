e3cad6e1cb9d2995a7975ffeac7a45289965ceaf
LEVENT DOLGUN <89582044+leventdolgun@users.noreply.github.com>
2024-12-28 11:39:49 +0300

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -16,29 +16,3 @@ const Todo = ({task, deleteTodo, editTodo}) => {
 };
 
 export default Todo;
-
-// import React from "react";
-// import { AiFillEdit } from "react-icons/ai";
-// import { BsFillTrash } from "react-icons/bs";
-
-// const Todo = ({ task, onEdit, onDelete }) => {
-//   return (
-//     <div className="flex items-center justify-between px-3 py-4 mb-1 text-white rounded-md cursor-pointer bg-violet-800">
-//       <p className="font-primary">{task.task}</p>
-//       <div className="flex items-center gap-x-4">
-//         <AiFillEdit
-//           className="text-xl cursor-pointer"
-//           onClick={() => onEdit(task.id)}
-//           aria-label="Edit Task"
-//         />
-//         <BsFillTrash
-//           className="text-xl cursor-pointer"
-//           onClick={() => onDelete(task.id)}
-//           aria-label="Delete Task"
-//         />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Todo;
